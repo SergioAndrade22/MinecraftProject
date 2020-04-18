4cb6d57649c1d6f5ce8f4a8da577a42c1ef86ab5
Sergio Andrade <sergio.david2201@gmail.com>
2020-04-18 20:35:58 -0300

    Initial commit

---
+++ src/main/java/com/hosuseri/tutorialmod/TutorialMod.java
@@ -0,0 +1,42 @@
+package com.hosuseri.tutorialmod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod("tutorialmod")
+public class TutorialMod
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    
+    public static final String mod_id = "tutorialmod";
+
+    public TutorialMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event){
+    	
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    	
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+    	
+    }
+}

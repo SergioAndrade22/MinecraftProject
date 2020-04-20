e022d477b20415cf80ac632a7a197624324938ed
Sergio Andrade <sergio.david2201@gmail.com>
2020-04-20 07:18:16 -0300

    Mode name refactored, "tutorialmod" to "aquaticworld"

---
+++ src/main/java/com/hosuseri/aquaticworld/AquaticWorld.java
@@ -0,0 +1,49 @@
+package com.hosuseri.aquaticworld;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("aquaticworld")
+public class AquaticWorld
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    
+    public static final String MOD_ID = "aquaticworld";
+    
+    public static AquaticWorld instance;
+
+    public AquaticWorld() {
+    	final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
+        modEventBus.addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        
+        instance = this;
+    }
+
+    private void setup(final FMLCommonSetupEvent event){
+    	
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    	
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+    	
+    }
+    
+    public static void log(String msg) {
+    	LOGGER.debug(msg);
+    }
+}
---
+++ src/main/java/com/hosuseri/aquaticworld/init/BlockInit.java
@@ -0,0 +1,56 @@
+package com.hosuseri.aquaticworld.init;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+
+import com.hosuseri.aquaticworld.AquaticWorld;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+@ObjectHolder(AquaticWorld.MOD_ID)
+@Mod.EventBusSubscriber(modid = AquaticWorld.MOD_ID, bus= Bus.MOD)
+public class BlockInit {
+	public static Block aquamarine_block = null;
+	public static BlockItem aquamarine_block_item = null;
+	
+	@SubscribeEvent
+	public static void registerBlocks(final RegistryEvent.Register<Block> event) { // for every block you register you also need to register the item for it
+		AquaticWorld.log("Begin blocks Registy");
+		
+		Block.Properties props = Block.Properties.create(Material.IRON);
+		props.hardnessAndResistance(7f, 15.0f);
+		props.sound(SoundType.METAL);
+		props.harvestTool(ToolType.PICKAXE);
+		props.harvestLevel(3);
+		aquamarine_block = new Block(props);
+		aquamarine_block.setRegistryName("aquamarine_block");
+		
+		event.getRegistry().register(aquamarine_block);
+		
+		AquaticWorld.log("Finish blocks Registry");
+	}
+	
+	@SubscribeEvent
+	public static void registerBlockItems(final RegistryEvent.Register<Item> event) {
+		AquaticWorld.log("Begin blocks Registy");
+		
+		Item.Properties props = new Item.Properties();
+		props.maxStackSize(16);
+		props.group(TutorialItemGroup.instance);
+		
+		aquamarine_block_item = new BlockItem(aquamarine_block, props);
+		aquamarine_block_item.setRegistryName("aquamarine_block"); 
+		
+		event.getRegistry().register(aquamarine_block_item);
+		
+		AquaticWorld.log("Finish blocks Registry");
+	}
+}
